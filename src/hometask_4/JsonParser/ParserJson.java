690b64a6cb98d1caa162477dd58ceccf9ca980a9
Gurmigou <egor555x6@gmail.com>
2021-09-12 16:09:24 +0300

    Json Parser workds correctly. A few bugs were fixed. Methods: jsonIsValid() and getJsonStatistics() added

--- src/hometask_4/JsonParser/ParserJson.java
+++ src/hometask_4/JsonParser/ParserJson.java
@@ -3,8 +3,23 @@ package hometask_4.JsonParser;
 import java.util.*;
 
 public class ParserJson {
-    private static final String strValueRegex = "^\".+\"\s*:\s*\".+\"\s*(.|\n)*";
+    private static final String strValueRegex = "^\"[^\"]+\"\s*:\s*\"[^\"]*\"\s*(.|\n)*";
     private static final String numberValueRegex = "^\".+\"\s*:\s*-?(?:0|[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+-]?\\d+)?\s*(.|\n)*";
+
+    private static final Statistics jsonStatistics = new Statistics();
+
+    private static class Statistics {
+        private int objectsNum;
+        private int arraysNum;
+
+        public int getObjectsNum() {
+            return objectsNum;
+        }
+
+        public int getArraysNum() {
+            return arraysNum;
+        }
+    }
 
     private static class Pair<T, Q> {
         T first;
@@ -49,7 +64,6 @@ public class ParserJson {
     }
 
     private int putKeyValue(String json, int from, Map<String, Object> map) throws InvalidJsonException {
-
         int i1 = json.indexOf('\"', from);
         int i2 = json.indexOf('\"', i1 + 1);
 
@@ -87,71 +101,114 @@ public class ParserJson {
         int leftQuote = -1;
         int rightQuote = -1;
 
-        char ch;
-        while ((ch = json.charAt(from)) != '}') {
+        // двоеточие
+        boolean colonOccurred = false;
+        char ch = 0;
+        while (from < json.length() && (ch = json.charAt(from)) != '}') {
             switch (ch) {
                 case '\"' -> {
                     String subJson = json.substring(from);
                     if (subJson.matches(strValueRegex) || subJson.matches(numberValueRegex))
                         from = putKeyValue(json, from, map);
+                    else if (leftQuote != -1 && rightQuote != -1)
+                        throw new InvalidJsonException("JSON object isn't correct");
                     else if (leftQuote == -1)
                         leftQuote = from;
                     else
                         rightQuote = from;
                 }
-                case '{', '['  -> {
-                    if (leftQuote == -1 || rightQuote == -1)
+                case '{', '[' -> {
+                    if (leftQuote == -1 || rightQuote == -1 || !colonOccurred)
                         throw new InvalidJsonException("JSON object isn't correct");
 
                     String key = json.substring(leftQuote + 1, rightQuote);
                     leftQuote = rightQuote = -1;
+                    colonOccurred = false;
 
                     if (ch == '{')
                         from = object(json, from, key, map, array, false);
                     else
                         from = array(json, from, key, map);
                 }
+                case ':' -> {
+                    if (leftQuote != -1 && rightQuote != -1)
+                        colonOccurred = true;
+                }
             }
             from++;
         }
+
+        if (ch != '}')
+            throw new InvalidJsonException("JSON object isn't correct");
+
         return from;
     }
 
-    private boolean areNumbers(String[] data) {
-        return data.length == Arrays.stream(data).
-                              filter(s -> {
-                                    for (int i = 0; i < s.length(); i++) {
-                                        if (!isNumber(s.charAt(i)))
-                                            return false;
-                                    }
-                                    return true;
-                              }).count();
+    private boolean isNumberCollection(String json, int from, int endOfArray) {
+        int quoteIndex = json.indexOf('\"', from);
+        return quoteIndex == -1 || quoteIndex > endOfArray;
     }
 
-    private int plainArrayData(String json, int from, List<Object> array) {
-        int endOfData = json.indexOf(']', from);
+    private String[] splitAsNumberData(String plainData) {
+        return plainData.split("\"\s*,\s*\"");
+    }
 
-        String subJson = json.substring(from, endOfData);
-        String[] data = subJson.split("[\n\s]*[,\\]}\s][\n\s]*");
-
-        if (areNumbers(data)) {
-            for (String num : data)
-                array.add(Double.parseDouble(num));
+    private String[] splitAsStringData(String plainData) {
+        String[] split = plainData.split("\"[\s\n]*,[\s\n]*\"");
+        int length = split.length;
+        switch (length) {
+            case 0 -> {
+                return split;
+            }
+            case 1 -> {
+                split[0] = split[0].substring(1, split[0].length() - 1);
+                return split;
+            }
+            default -> {
+                split[0] = split[0].substring(1);
+                String lastWords = split[split.length - 1];
+                split[split.length - 1] = lastWords.substring(0, lastWords.indexOf("\""));
+                return split;
+            }
         }
-        else
-            array.addAll(Arrays.asList(data));
+    }
 
-        return endOfData;
+    private int plainArrayData(String json, int from, List<Object> array) throws InvalidJsonException {
+        int endOfArray = json.indexOf(']', from);
+        if (endOfArray == -1)
+            throw new InvalidJsonException("JSON object isn't correct");
+
+        if (isNumberCollection(json, from, endOfArray)) {
+            String subJson = json.substring(from, endOfArray);
+            try {
+                Arrays.stream(splitAsNumberData(subJson))
+                      .forEach(s -> array.add(Double.parseDouble(s)));
+            } catch (Exception e) {
+                throw new InvalidJsonException("JSON object isn't correct");
+            }
+        }
+        else {
+            String subJson = json.substring(from, endOfArray + 1); // in order to include ']'
+
+            if (subJson.matches("(\"[^\"]+\"[\s|\n]*[,|\\]][\s|\n]*)*"))
+                array.addAll(Arrays.asList(splitAsStringData(subJson)));
+            else
+                throw new InvalidJsonException("JSON object isn't correct");
+        }
+        return endOfArray;
     }
 
     private int array(String json, int from, String key, Map<String, Object> map)
             throws InvalidJsonException
     {
+        // statistics
+        jsonStatistics.arraysNum++;
+
         var array = new ArrayList<>();
         map.put(key, array);
 
         char ch;
-        while ((ch = json.charAt(from)) != ']') {
+        while (from < json.length() && (ch = json.charAt(from)) != ']') {
             if (ch == '{')
                 from = object(json, from, null, null, array, false);
             else if (ch == '\"' || isNumber(ch))
@@ -160,12 +217,19 @@ public class ParserJson {
                 from++;
         }
 
+        // json object can't end with character ']'
+        if (from == json.length())
+            throw new InvalidJsonException("JSON object isn't correct");
+
         return from;
     }
 
     private int object(String json, int from, String key, Map<String, Object> map, List<Object> array, boolean init)
             throws InvalidJsonException
     {
+        // statistics
+        jsonStatistics.objectsNum++;
+
         if (!init && (key != null || array != null)) {
             var innerMap = new HashMap<String, Object>();
             if (key != null)
@@ -175,28 +239,49 @@ public class ParserJson {
             map = innerMap;
         }
 
-        while (json.charAt(from) != '}') {
-            if (json.charAt(from) == '\"')
+        char ch = 0;
+        while (from < json.length() && (ch = json.charAt(from)) != '}') {
+            if (ch == '\"')
                 from = keyValue(json, from, map, array);
-            else
+            else if (ch == ' ' || ch == '\n' || ch == '{')
                 from++;
+            else
+                throw new InvalidJsonException("JSON object isn't correct");
         }
+
+        if (ch != '}')
+            throw new InvalidJsonException("JSON object isn't correct");
 
         return from;
     }
 
-    public static Map<String, Object> parse(String json) throws InvalidJsonException {
+    public static Map<String, Object> parseJson(String json) throws InvalidJsonException {
         var parser = new ParserJson();
         var map = new HashMap<String, Object>();
         parser.object(json, 0, null, map, null, true);
         return map;
     }
 
+    public static boolean jsonIsValid(String json) {
+        try {
+            parseJson(json);
+            return true;
+        } catch (InvalidJsonException e) {
+            return false;
+        }
+    }
 
+    public static Statistics getJsonStatistics(String json) throws InvalidJsonException {
+        parseJson(json);
+        return jsonStatistics;
+    }
+
+    // TEST
     public static void main(String[] args) throws InvalidJsonException {
-        String json = """
+        String json =
+                """
                 {
-                  squadName": "Super hero squad",
+                  "squadName": "Super hero squad",
                   "homeTown": "Metro City",
                   "formed": 2016,
                   "secretBase": "Super tower",
@@ -235,7 +320,7 @@ public class ParserJson {
                     }
                   ]
                 }
-                    """;
-        Map<String, Object> parsedJson = parse(json);
+                """;
+        Map<String, Object> parsedJson = parseJson(json);
     }
 }
